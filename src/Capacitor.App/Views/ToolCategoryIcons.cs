95667b8f040c3db2893a265334814aa328825e9c
Norton Andreev <norton@nortonandreev.com>
2026-09-21 16:17:38 +0500

    Left-align system chat cards and wire kind headers (#1083)

--- src/Capacitor.App/Views/ToolCategoryIcons.cs
+++ src/Capacitor.App/Views/ToolCategoryIcons.cs
@@ -27,4 +27,8 @@ public static class ToolCategoryIcons {
         "Question"   => ForCategory(ToolCategory.Question),
         _            => "",
     };
+
+    public static string NoteIcon => ForFixedLabel("Note");
+    public static string PermissionIcon => ForFixedLabel("Permission");
+    public static string QuestionIcon => ForFixedLabel("Question");
 }
